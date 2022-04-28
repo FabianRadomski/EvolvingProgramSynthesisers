6e8ba3bbb87948693ef0e5b59024bf497b5f4539
Philip Tempelman <tempelmanphilip@gmail.com>
2022-04-28 18:40:22 +0200

    add WIP fitness functionality and helper methods

--- metasynthesis/programming_language/evolving_language.py
+++ metasynthesis/programming_language/evolving_language.py
@@ -1,5 +1,19 @@
+from itertools import chain
 from random import randrange
-from typing import List, Callable, Tuple
+from typing import List, Callable, Tuple, Iterable
+from common.tokens.pixel_tokens import BoolTokens as PixelBool
+from common.tokens.robot_tokens import BoolTokens as RobotBool
+from common.tokens.string_tokens import BoolTokens as StringBool
+
+from evaluation.experiment_procedure import test_performance_single_case_and_write_to_file
+from example_parser.pixel_parser import PixelParser
+from example_parser.robot_parser import RobotParser
+from example_parser.string_parser import StringParser
+from example_parser.parser import Parser
+from common.experiment import Experiment
+
+from search.brute.brute import Brute
+from search.batch_run import BatchRun
 
 from metasynthesis.abstract_genetic import GeneticAlgorithm
 
@@ -16,19 +30,22 @@ class EvolvingLanguage(GeneticAlgorithm):
     """A genetic algorithm to synthesise a programing language for program synthesis"""
 
     def __init__(self, fitness_limit: int, generation_limit: int, crossover_probability: float,
-                 mutation_probability: float, domain: str, all_tokens: list):
+                 mutation_probability: float, domain: str, bool_tokens: list, trans_tokens: list):
         super().__init__(fitness_limit, generation_limit, crossover_probability, mutation_probability)
         self.domain = domain
-        self.all_tokens = all_tokens
+        self.bool_tokens = bool_tokens
+        self.trans_tokens = trans_tokens
 
     def generate_genome(self, length: int) -> Genome:
         """This method creates a new genome of the specified length"""
 
+        all_tokens = self.bool_tokens + self.trans_tokens
+
         genome = set()
 
         for i in range(0, length):
-            random_index = randrange(0, len(self.all_tokens))
-            random_token = self.all_tokens[random_index]
+            random_index = randrange(0, len(all_tokens))
+            random_token = all_tokens[random_index]
             genome.add(random_token)
 
         return sort_genome(list(genome))
@@ -36,10 +53,12 @@ class EvolvingLanguage(GeneticAlgorithm):
     def generate_population(self, size: int) -> Population:
         """This method creates a population of new genomes"""
 
+        all_tokens = self.bool_tokens + self.trans_tokens
+
         population = []
 
         for i in range(0, size):
-            genome_length = randrange(1, len(self.all_tokens))
+            genome_length = randrange(1, len(all_tokens))
             population.append(self.generate_genome(genome_length))
 
         return population
@@ -47,7 +66,44 @@ class EvolvingLanguage(GeneticAlgorithm):
     def fitness(self, genome: Genome) -> float:
         """This method calculates the fitness of the specified genome"""
 
-        raise NotImplementedError()
+        # need a combination of
+        # 1) length of genome
+        # 2) whether program that solves tasks was created
+        # 3) number of iterations of searching
+
+        genome_length = len(genome)
+
+        # result = BatchRun(
+        #     domain=self.domain,
+        #     files=([], [], []),
+        #     search_algorithm=[Brute, "brute"][0](10),
+        #     print_results=False,
+        #     multi_core=False
+        # ).run()
+        # from result we can get the relevant stats
+        # print("AAAA", result)
+
+        # ISSUE: we need a way to inject the DSL genome into BatchRun
+
+        # alternative: use test cases to run experiments myself
+
+        # ISSUE: we can inject a DSL, but runs very very slow
+        filtered_tokens = filter_tokens(genome, self.domain)
+        bool_tokens = filtered_tokens[0]
+        trans_tokens = filtered_tokens[1]
+
+        parser = _get_parser(self.domain)
+        test_cases = parser.parse_specific_range([], [], [])
+
+        for test_case in test_cases:
+            succes, time = test_performance_single_case_and_write_to_file(test_case, trans_tokens, bool_tokens, Brute)
+            print(succes, time)
+
+        # ISSUE: this runs quite fast, but no way to inject custom DSL
+        # experiment = Experiment("temp_experiment_name", self.domain, test_cases)
+        # print(test_performance_single_experiment(experiment, Brute))
+
+        return 0.5
 
     def single_point_crossover(self, a: Genome, b: Genome) -> Tuple[Genome, Genome]:
         """This method combines first half of genome a with second half of b and vice versa with certain probability"""
@@ -82,11 +138,51 @@ class EvolvingLanguage(GeneticAlgorithm):
     def run_evolution(self) -> Tuple[Population, int]:
         """This method runs the evolution process"""
 
-        print(self.generate_population(100))
+        iteration_count = 0
 
-        return self.generate_population(100), 1
+        population = self.generate_population(50)
+
+        print(population)
+
+        # for genome in population:
+        #     print(self.fitness(genome))
+
+        return self.generate_population(50), iteration_count
 
 
-def sort_genome(genome: Genome):
+def sort_genome(genome: Genome) -> Genome:
     return sorted(genome, key=str, reverse=False)
 
+
+def filter_tokens(genome: Genome, domain: str) -> Tuple[List, List]:
+    filtered = ([], [])
+    if domain == "pixel":
+        for token in genome:
+            if token in PixelBool:
+                filtered[0].append(token)
+            else:
+                filtered[1].append(token)
+    elif domain == "robot":
+        for token in genome:
+            if token in RobotBool:
+                filtered[0].append(token)
+            else:
+                filtered[1].append(token)
+    elif domain == "string":
+        for token in genome:
+            if token in StringBool:
+                filtered[0].append(token)
+            else:
+                filtered[1].append(token)
+    return filtered
+
+
+def _get_parser(domain: str) -> Parser:
+    if domain == "string":
+        return StringParser()
+    elif domain == "robot":
+        return RobotParser()
+    elif domain == "pixel":
+        return PixelParser()
+    else:
+        raise Exception()
--- metasynthesis/programming_language/run_genetic.py
+++ metasynthesis/programming_language/run_genetic.py
@@ -6,10 +6,11 @@ domain_name = "robot"
 # domain_name = "pixel"
 # domain_name = "string"
 
-all_tokens = list(extract_bool_tokens_from_domain_name(domain_name)
-                  .union(extract_trans_tokens_from_domain_name(domain_name)))
+bool_tokens = list(extract_bool_tokens_from_domain_name(domain_name))
+trans_tokens = list(extract_trans_tokens_from_domain_name(domain_name))
 
-genetic = EvolvingLanguage(1, 100, 0.5, 0.5, domain_name, all_tokens)
+
+genetic = EvolvingLanguage(1, 100, 0.5, 0.5, domain_name, bool_tokens, trans_tokens)
 
 genetic.run_evolution()
 
