ed0dc9ae81abfb1151a06bbb78844632cfd0050a
lgkroes <lucas.kroes@hetnet.nl>
2022-05-09 14:40:50 +0200

    Add constraint function to domain specific language for genetic algorithm.

--- common/program_synthesis/dsl.py
+++ common/program_synthesis/dsl.py
@@ -1,10 +1,11 @@
 import common.tokens.robot_tokens as robot_tokens
 import common.tokens.pixel_tokens as pixel_tokens
 import common.tokens.string_tokens as string_tokens
-from typing import List, Type
+from typing import List, Type, Callable
 
 from common.tokens.abstract_tokens import Token
 
+ConstraintFunc = Callable[[List[Token]], List[Token]]
 
 class DomainSpecificLanguage:
     """A class for a domain specific language"""
@@ -13,19 +14,21 @@ class DomainSpecificLanguage:
                  domain_name: str,
                  bool_tokens: List[Type[Token]],
                  trans_tokens: List[Type[Token]],
-                 constraints_enabled: bool = False):
+                 constraints_enabled: bool = False,
+                 constraint_func: ConstraintFunc = lambda x: x):
         self.domain_name = domain_name
         self._bool_tokens = bool_tokens
         self._trans_tokens = trans_tokens
         self._constraints_enabled = constraints_enabled
+        self._constraint_func = constraint_func
 
     def get_trans_tokens(self, partial_program=None) -> List[Type[Token]]:
         """This method gets trans tokens"""
-
+        if not self._constraints_enabled:
+            return self._trans_tokens
         if partial_program is None:
             partial_program = []
-
-        return self._trans_tokens
+        return self._constraint_func[partial_program]
 
     def get_bool_tokens(self):
         """This method gets bool tokens"""
