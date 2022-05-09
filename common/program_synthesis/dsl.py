dfbd54fc16e5484b59d21e63e41d57fbda136483
lgkroes <lucas.kroes@hetnet.nl>
2022-05-09 15:52:48 +0200

    Add Identity function for DomainSpecificLanguage

--- common/program_synthesis/dsl.py
+++ common/program_synthesis/dsl.py
@@ -7,28 +7,31 @@ from common.tokens.abstract_tokens import Token
 
 ConstraintFunc = Callable[[List[Token]], List[Token]]
 
+def identity(x):
+    return x
+
 class DomainSpecificLanguage:
     """A class for a domain specific language"""
 
     def __init__(self,
                  domain_name: str,
-                 bool_tokens: List[Type[Token]],
-                 trans_tokens: List[Type[Token]],
+                 bool_tokens: List[Token],
+                 trans_tokens: List[Token],
                  constraints_enabled: bool = False,
-                 constraint_func: ConstraintFunc = lambda x: x):
+                 constraint_func: ConstraintFunc = identity):
         self.domain_name = domain_name
         self._bool_tokens = bool_tokens
         self._trans_tokens = trans_tokens
         self._constraints_enabled = constraints_enabled
         self._constraint_func = constraint_func
 
-    def get_trans_tokens(self, partial_program=None) -> List[Type[Token]]:
+    def get_trans_tokens(self, partial_program=None) -> List[Token]:
         """This method gets trans tokens"""
         if not self._constraints_enabled:
             return self._trans_tokens
         if partial_program is None:
             partial_program = []
-        return self._constraint_func[partial_program]
+        return self._constraint_func(partial_program)
 
     def get_bool_tokens(self):
         """This method gets bool tokens"""
