51946e333b296333110b10b1c0420255d9898bea
Nikolaos Efthymiou <n.efthymiou@student.tudelft.nl>
2022-05-25 15:35:01 +0200

    update ga

--- metasynthesis/performance_function/evolving_function.py
+++ metasynthesis/performance_function/evolving_function.py
@@ -1,13 +1,20 @@
+import time
+from operator import add
 from typing import Tuple, List
-from random import randint, random
+from random import randint, random, choice
 import numpy as np
 
+from common.environment import RobotEnvironment
+from common.program_synthesis.dsl import StandardDomainSpecificLanguage
+from common.program_synthesis.objective import ObjectiveFun
+from common.program_synthesis.runner import Runner
 from metasynthesis.abstract_genetic import GeneticAlgorithm, MutationFunc, CrossoverFunc
 from metasynthesis.performance_function.dom_dist_fun.pixel_dist_fun import PixelDistFun
 from metasynthesis.performance_function.dom_dist_fun.robot_dist_fun import RobotDistFun
 from metasynthesis.performance_function.dom_dist_fun.string_dist_fun import StringDistFun
 from metasynthesis.performance_function.expr_tree import ExpressionTree, printN
-from metasynthesis.performance_function.symbol import TermSym
+from metasynthesis.performance_function.symbol import TermSym, OpSym
+from search.brute.brute import Brute
 
 Genome = ExpressionTree
 Population = List[Genome]
@@ -17,19 +24,19 @@ class EvolvingFunction(GeneticAlgorithm):
 
     def __init__(self, fitness_limit: int, generation_limit: int, crossover_probability: float,
                  mutation_probability: float, generation_size: int,
-                 domain_name: str, tournament_size: int = 2, elite_size: int = 6):
+                 domain: str, tournament_size: int = 2, elite_size: int = 6):
         assert (generation_size % 2 == 0), "population size (generation_size) should be even"
         assert (elite_size % 2 == 0) and elite_size < generation_size, "elite size (elite_size) should be even and (< gen_size)"
         super().__init__(fitness_limit=fitness_limit, generation_limit=generation_limit,
                          crossover_probability=crossover_probability,
                          mutation_probability=mutation_probability, generation_size=generation_size)
-        self.domain = domain_name
+        self.domain = domain
         self.terms = None
-        if domain_name == 'robot':
+        if domain == 'robot':
             partial_dist_funs = RobotDistFun.partial_dist_funs()
-        elif domain_name == 'pixel':
+        elif domain == 'pixel':
             partial_dist_funs = PixelDistFun.partial_dist_funs()
-        elif domain_name == 'string':
+        elif domain == 'string':
             partial_dist_funs = StringDistFun.partial_dist_funs()
         else:
             raise Exception('Domain should be one of: robot, string, pixel.')
@@ -40,11 +47,13 @@ class EvolvingFunction(GeneticAlgorithm):
     def generate_genome(self, max_depth=4) -> Genome:
         return ExpressionTree.generate_random_expression(terms=self.terms, max_depth=max_depth)
 
-    def generate_population(self) -> Population:
-        return [self.generate_genome() for _ in range(self.generation_size)]
+    def generate_population(self, max_depth=4) -> Population:
+        return [self.generate_genome(max_depth=max_depth) for _ in range(self.generation_size)]
 
     def fitness(self, genome: Genome) -> float:
-        raise NotImplementedError()
+        # data = Runner(StandardDomainSpecificLanguage(self.domain), Brute(0.1, genome.distance_fun)).run()
+        # return data['average_success']
+        return randint(0, 100)
 
     def crossover(self, a: Genome, b: Genome, func: CrossoverFunc = None) -> Tuple[Genome, Genome]:
         if random() < self.crossover_probability:
@@ -59,37 +68,63 @@ class EvolvingFunction(GeneticAlgorithm):
             return genome
 
     def pop_fitness(self, pop: Population):
-        # return list(map(lambda x: self.fitness(x), pop))
-        return list(map(lambda x: randint(0, 10), pop))
+        # print('Calculating population fitness...')
+        start_time = time.time()
+        res = list(map(lambda x: self.fitness(x), pop))
+        # print("--- %s seconds taken to calculate pop. fitness: ---" % (time.time() - start_time))
+        return res
+        # return list(map(lambda x: randint(0, 10), pop))
 
-    def selection_pair(self, population: Population) -> Tuple[Genome, Genome]:
-        fitness_values = self.pop_fitness(population)
+    def selection_pair(self, population: Population, fitness_values: List[float]) -> Tuple[Genome, Genome]:
 
-        def tournament():
-            n = len(fitness_values)
-            best = None
-            for _ in range(self.tournament_size):
-                idx = randint(0, n - 1)
-                if (best is None) or fitness_values[idx] > fitness_values[best]:
-                    best = idx
-            return best
+        def tournament(idx_to_exclude: int):
+            if idx_to_exclude == -1:  # first parent
+                n = len(fitness_values)
+                best = None
+                for _ in range(self.tournament_size):
+                    idx = randint(0, n - 1)
+                    if (best is None) or fitness_values[idx] > fitness_values[best]:
+                        best = idx
+                return best
+            else:
+                # the first parent should not be considered again
+                n = len(fitness_values)
+                best = None
+                updated = list(range(n))
+                updated.remove(idx_to_exclude)
+                for _ in range(self.tournament_size):
+                    idx = choice(updated)
+                    if (best is None) or fitness_values[idx] > fitness_values[best]:
+                        best = idx
+                return best
 
-        parents_idx = [tournament() for _ in range(2)]
-        return population[parents_idx[0]], population[parents_idx[1]]
+        p1_idx = tournament(idx_to_exclude=-1)
+        p2_idx = tournament(idx_to_exclude=p1_idx)
 
-    def parent_selection(self, population: Population, num_pairs: int) -> List[Tuple[Genome, Genome]]:
-        return [self.selection_pair(population) for _ in range(num_pairs)]
+        # parents_idx = [tournament() for _ in range(2)]
+        return population[p1_idx], population[p2_idx]
+
+    def parent_selection(self, population: Population, fitness_values, num_pairs: int) -> List[Tuple[Genome, Genome]]:
+        return [self.selection_pair(population, fitness_values) for _ in range(num_pairs)]
 
     def genome_to_string(self, genome: Genome) -> str:
         return str(genome)
 
-    def run_evolution(self) -> Tuple[List[float], List[float], Genome]:
+    def run_evolution(self, verbose=False) -> Tuple[List[float], List[float], Genome]:
         population = self.generate_population()
         avg_fit: List[float] = []
         best_fit: List[float] = []
         best_individual: Genome = None
-        for _ in range(self.generation_limit):
+        for cur_gen in range(self.generation_limit):
+            # print('Starting generation no:', cur_gen)
             fitness_values = np.array(self.pop_fitness(population))
+            heights = np.array([x.height() for x in population])
+            if verbose:
+                print('Average height of generation:', cur_gen, ':', np.mean(heights))
+                print('MIN height', np.min(heights))
+                print('MAX height', np.max(heights))
+                print('---------------')
+
 
             ind_ranked = np.argsort(-fitness_values)
             top_k_ind = ind_ranked[:self.elite_size]
@@ -105,8 +140,8 @@ class EvolvingFunction(GeneticAlgorithm):
                 offspring.append(population[top_k_ind[i]])
                 i += 1
 
-            for parent_pair in self.parent_selection(population=population,
-                                                 num_pairs=int((self.generation_size - self.elite_size) / 2)):
+            for parent_pair in self.parent_selection(population=population, fitness_values=fitness_values,
+                                                     num_pairs=int((self.generation_size - self.elite_size) / 2)):
                 p1, p2 = parent_pair[0], parent_pair[1]
                 c1, c2 = self.crossover(p1, p2)
                 c1_prime, c2_prime = self.mutation(genome=c1), self.mutation(genome=c2)
@@ -117,23 +152,111 @@ class EvolvingFunction(GeneticAlgorithm):
         return avg_fit, best_fit, best_individual
 
 
+def distance_manually_designed_function(env1: RobotEnvironment, env2: RobotEnvironment) -> float:
+    def d(xy1: 'tuple[int, int]', xy2: 'tuple[int, int]'):
+        return abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
+
+    # position robot and position ball
+    pr = (env1.rx, env1.ry)
+    pb = (env1.bx, env1.by)
+
+    # position goal robot and position goal bal
+    prstar = (env2.rx, env2.ry)
+    pbstar = (env2.bx, env2.by)
+
+    if d(pb, pbstar) == 0:
+        if d(pr, prstar) == 0:
+            if env1.holding == env2.holding:
+                return 0
+            else:
+                return 1
+        else:
+            if env2.holding:
+                return 1 + d(pr, prstar)
+            else:
+                return d(pr, prstar)
+    else:
+        if env1.holding:
+            if not env2.holding:
+                return 1 + d(pr, pbstar) + d(pbstar, prstar)
+            else:
+                return d(pr, pbstar)
+        else:
+            if not env2.holding:
+                return 2 + d(pbstar, prstar) + d(pr, pb) + d(pb, pbstar)
+            else:
+                return 1 + d(pr, pb) + d(pb, pbstar)
+
+
+
+def distance_default_expr_tree(env1: RobotEnvironment, env2: RobotEnvironment) -> float:
+    def d(xy1: 'tuple[int, int]', xy2: 'tuple[int, int]'):
+        return abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
+
+    # position robot and position ball
+    pr = (env1.rx, env1.ry)
+    pb = (env1.bx, env1.by)
+
+    # position goal robot and position goal bal
+    pgr = (env2.rx, env2.ry)
+    pgb = (env2.bx, env2.by)
+
+    partial_dist_funs = RobotDistFun.partial_dist_funs()
+    terms = list(map(lambda x: TermSym(x), partial_dist_funs))
+
+    if pr != pb and pb != pgb:
+        root = ExpressionTree(OpSym(add), ExpressionTree(OpSym(add), ExpressionTree(terms[3], None, None),
+                                                         ExpressionTree(terms[4], None, None)),
+                              ExpressionTree(terms[2], None, None))
+        return root.distance_fun(env1, env2) + 2
+        # return d(pr, pb) + d(pb, pgb) + d(pgb, pgr) + 2
+    elif pr == pb and pb != pgb:
+        root = ExpressionTree(OpSym(add), ExpressionTree(terms[0], None, None), ExpressionTree(terms[2], None, None))
+        return root.distance_fun(env1, env2) + 1
+        # return d(pr, pgb) + d(pgb, pgr) + 1
+    else:
+        root = ExpressionTree(terms[1], None, None)
+        return root.distance_fun(env1, env2)
+        # return d(pr, pgr)
+
+
+def rand_env() -> RobotEnvironment:
+    size = 5
+    rx = randint(0, size - 1)
+    ry = randint(0, size - 1)
+    bx = randint(0, size - 1)
+    by = randint(0, size - 1)
+    return RobotEnvironment(size=size, rx=rx, ry=ry, bx=bx, by=by, holding=False)
+
+
+def test_default_vs_default_expr_tree():
+    count = 0
+    ct_1 = 0
+    ct_2 = 0
+    for i in range(10000):
+        env1 = rand_env()
+        env2 = rand_env()
+        t1 = time.time()
+        d1 = ObjectiveFun("robot").fun(env1, env2)
+        ct_1 += time.time() - t1
+        t2 = time.time()
+        d2 = distance_default_expr_tree(env1, env2)
+        ct_2 += time.time() - t2
+        if d1 == d2:
+            count += 1
+    print(count == 10000)
+    print(ct_1, ct_2)
+
+
 if __name__ == '__main__':
-    ga = EvolvingFunction(fitness_limit=0, generation_limit=100, crossover_probability=0.7,
-                          mutation_probability=0.01, generation_size=30, domain_name='robot', tournament_size=3, elite_size=6)
-    a = ga.generate_genome(max_depth=2)
-    b = ga.generate_genome(max_depth=2)
-    print(a)
-    print(b)
-    c1, c2 = ga.crossover(a=a, b=b)
-    print(c1)
-    print(c2)
-    print(ga.mutation(genome=c2))
-    selected1, selected2 = ga.selection_pair(ga.generate_population())
-    print(selected1, '\n', selected2)
-
-    par_sel = ga.parent_selection(ga.generate_population(), num_pairs=50)
-    print(len(par_sel))
-    for pair in par_sel:
-        print(pair[0], '---------', pair[1])
-
-    print(ga.run_evolution())
+    ga = EvolvingFunction(fitness_limit=0, generation_limit=20, crossover_probability=0.7,
+                          mutation_probability=0.01, generation_size=40, domain='robot',
+                          tournament_size=3, elite_size=2)
+    avg_fit, best_fit, best_individual = ga.run_evolution()
+    print(avg_fit)
+    print(best_fit)
+    print(best_individual)
+    # T = time.time()
+    # print(ga.fitness(ga.generate_genome(max_depth=2)))
+    # print('TIME TAKEN:', time.time() - T)
+    # test_default_vs_default_expr_tree()
--- metasynthesis/performance_function/expr_tree.py
+++ metasynthesis/performance_function/expr_tree.py
@@ -3,6 +3,7 @@ import sys
 from operator import mul, add, sub, truediv as div
 from random import choice
 from typing import List, Tuple
+import numpy as np
 
 from common.environment import Environment
 from metasynthesis.performance_function.dom_dist_fun.robot_dist_fun import RobotDistFun
@@ -39,19 +40,40 @@ class ExpressionTree:
     def distance_fun(self, x: Environment, y: Environment) -> float:
         return abs(self.eval(x=x, y=y))
 
-    def random_walk_helper(self, cur_depth, min_depth, traversal) -> Tuple[List[int], 'ExpressionTree']:
+    def random_walk_bernoulli_helper(self, cur_depth, min_depth, traversal) -> Tuple[List[int], 'ExpressionTree']:
         # returns a random subtree
-        stop: bool = get_next_bernoulli(p=0.5)  # true with probability p
+        stop: bool = get_next_bernoulli(p=0.3)  # true with probability p
         if (stop and (cur_depth >= min_depth)) or (not self.symbol.is_operator()):
             # stop if (stop is true and (cur_depth >= min_depth)) or we reached a leaf
             return traversal, self
         else:
             pick_child, idx = (self.left, 0) if get_next_bernoulli(p=0.5) else (self.right, 1)
             traversal.append(idx)
-            return pick_child.random_walk_helper(cur_depth=cur_depth + 1, min_depth=min_depth, traversal=traversal)
+            return pick_child.random_walk_bernoulli_helper(cur_depth=cur_depth + 1, min_depth=min_depth,
+                                                           traversal=traversal)
 
-    def random_walk(self, min_depth=0) -> Tuple[List[int], 'ExpressionTree']:
-        return self.random_walk_helper(cur_depth=0, min_depth=min_depth, traversal=[])
+    def random_walk_bernoulli(self, min_depth=0) -> Tuple[List[int], 'ExpressionTree']:
+        return self.random_walk_bernoulli_helper(cur_depth=0, min_depth=min_depth, traversal=[])
+
+    def random_walk_uniform_depth_helper(self, min_depth) -> Tuple[List[int], 'ExpressionTree']:
+        # returns a random subtree
+        length_of_traversal = random.randint(min_depth, self.height())
+        if length_of_traversal == 0:
+            return [], copy.deepcopy(self)
+        rand_traversal = np.random.randint(0, 2, (length_of_traversal,))
+        result = copy.deepcopy(self)
+        for idx, dir in enumerate(list(rand_traversal)):
+            if not result.symbol.is_operator():
+                return rand_traversal[:idx], result
+            else:
+                if dir == 1:
+                    result = result.right
+                else:
+                    result = result.left
+        return rand_traversal, result
+
+    def random_walk_uniform_depth(self, min_depth=0) -> Tuple[List[int], 'ExpressionTree']:
+        return self.random_walk_uniform_depth_helper(min_depth=min_depth)
 
     def get_ops_helper(self, traversal: List[int]) -> List[Tuple[List[int], 'ExpressionTree']]:
         if self.symbol.is_operator():
@@ -80,7 +102,7 @@ class ExpressionTree:
         # b) pick a random term (leaf) and change it
         # c) TODO: pick a random term (leaf) and replace it with a random operator with two random terms as children
         clone = copy.deepcopy(self)
-        traversal, random_node = clone.random_walk(min_depth=0)
+        traversal, random_node = clone.random_walk_uniform_depth(min_depth=0)
         if random_node.symbol.is_operator():
             if verbose:
                 print('Changing:', random_node.symbol)
@@ -99,8 +121,9 @@ class ExpressionTree:
                 print('New node:', random_node.symbol)
         return clone
 
-    def reproduce_with(self, other: 'ExpressionTree', verbose=False) -> Tuple['ExpressionTree', 'ExpressionTree']:
-        traversal_t, t = self.random_walk(min_depth=1)
+    def reproduce_with_t_with_t_op_with_op(self, other: 'ExpressionTree', verbose=False) -> Tuple['ExpressionTree', 'ExpressionTree']:
+        # terms are replaced only with terms, operators are replaced only with operators
+        traversal_t, t = self.random_walk_uniform_depth()
         if verbose:
             print('Traversal in T:', traversal_t)
             print('Node to be replaced:', t)
@@ -113,6 +136,20 @@ class ExpressionTree:
             print('Traversal in T\'', traversal_t_prime)
             print('Random node in other:', random_symbol)
         c1 = self.replace_branch(traversal=traversal_t, replace_with=random_symbol)
+        c2 = other.replace_branch(traversal=traversal_t_prime, replace_with=t)
+        return c1, c2
+
+    def reproduce_with(self, other: 'ExpressionTree', verbose=False) -> Tuple['ExpressionTree', 'ExpressionTree']:
+        traversal_t, t = self.random_walk_uniform_depth()
+        if verbose:
+            print('Traversal in T:', traversal_t)
+            print('Node to be replaced:', t)
+        # term/op can be replace with term/op
+        traversal_t_prime, random_subtree = other.random_walk_uniform_depth(min_depth=0)
+        if verbose:
+            print('Traversal in T\'', traversal_t_prime)
+            print('Random node in other:', random_subtree)
+        c1 = self.replace_branch(traversal=traversal_t, replace_with=random_subtree)
         c2 = other.replace_branch(traversal=traversal_t_prime, replace_with=t)
         return c1, c2
 
@@ -135,6 +172,12 @@ class ExpressionTree:
                 return ExpressionTree(self.symbol, self.left,
                                       self.right.replace_branch(traversal=traversal[1:], replace_with=replace_with))
 
+    def height(self):
+        if self.symbol.is_operator():
+            return max(self.left.height(), self.right.height()) + 1
+        else:
+            return 0
+
     @staticmethod
     def generate_random_expression(terms: List[TermSym],
                                    operators: List[OpSym] = [OpSym(add), OpSym(sub), OpSym(mul), OpSym(div)],
@@ -156,7 +199,7 @@ def helper_generator(terms, operators, cur_depth, max_depth) -> ExpressionTree:
         return ExpressionTree(symbol=choice(terms), left=None, right=None)
     else:
         # min depth = 1
-        probability_of_operator = 1 if (cur_depth == 0) else 0.5
+        probability_of_operator = 1 if (cur_depth == 0) else 0.6  # TODO: tune this probability
         bernoulli = get_next_bernoulli(probability_of_operator)
         if bernoulli:
             symbol = choice(operators)
@@ -168,3 +211,23 @@ def helper_generator(terms, operators, cur_depth, max_depth) -> ExpressionTree:
             return ExpressionTree(symbol=symbol,
                                   left=None,
                                   right=None)
+
+
+def generate_complete_tree(terms, operators, cur_depth, max_depth) -> ExpressionTree:
+    if cur_depth == max_depth:
+        return ExpressionTree(symbol=choice(terms), left=None, right=None)
+    else:
+        symbol = choice(operators)
+        return ExpressionTree(symbol=symbol,
+                              left=generate_complete_tree(terms, operators, cur_depth + 1, max_depth),
+                              right=generate_complete_tree(terms, operators, cur_depth + 1, max_depth))
+
+
+def generate_high_tree(terms, operators, cur_depth, max_depth) -> ExpressionTree:
+    if cur_depth == max_depth:
+        return ExpressionTree(symbol=choice(terms), left=None, right=None)
+    else:
+        symbol = choice(operators)
+        return ExpressionTree(symbol=symbol,
+                              left=generate_high_tree(terms, operators, cur_depth + 1, max_depth),
+                              right=ExpressionTree(symbol=choice(terms), left=None, right=None))
