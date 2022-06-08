c0da07e2cdb8b0c9b37d567eb92df0e69aec6a22
MichalOkon <michalionis123@gmail.com>
2022-06-08 19:12:56 +0200

    Add support for running a single procedure

--- run_single.py
+++ run_single.py
@@ -12,6 +12,11 @@ if __name__ == "__main__":
 
     store = False if test_cases == "param" else store
 
-    mean1 = Runner(dicts(0), algo, setting, test_cases, time_limit, debug, store).run()
-
-    print(f"Solved {str(mean1)}")
+    res1 = Runner(dicts(0), "AS", setting, test_cases, 26.3, debug, store).run()
+    res2 = Runner(dicts(0), "AS", setting, test_cases, 26.4, debug, store).run()
+    res3 = Runner(dicts(0), "MH", setting, test_cases, 20.3, debug, store).run()
+    res4 = Runner(dicts(0), "MH", setting, test_cases, 19.5, debug, store).run()
+    print(f"1.AStar 26.3 Solved {str(res1[0])}, time: {str(res1[1])}\n")
+    print(f"2.AStar 26.4 Solved {str(res2[0])}, time: {str(res2[1])}\n")
+    print(f"3.MH 20.3 Solved {str(res3[0])}, time: {str(res3[1])}\n")
+    print(f"4.MH 19.5 Solved {str(res4[0])}, time: {str(res4[1])}\n")
